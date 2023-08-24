00289d2a588bf61a5c34189f57c217f19593920e
cdschill <cdschill@gmail.com>
2023-08-24 16:46:47 -0400

    Updated setupa nd added a method

---
+++ pypack/__init__.py
@@ -0,0 +1,2 @@
+def test_method(text)
+    print('This is the text; ', text)
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pypack',
+    version='0.0.1',
+    description='My private package from private github repo',
+    url='git@github.com:rfschubert/pypack-sdk-package.git',
+    author='Raphael Schubert',
+    author_email='raphael.schubert@digitalbankscorp.com',
+    license='unlicense',
+    packages=['pypack'],
+    zip_safe=False
+)
